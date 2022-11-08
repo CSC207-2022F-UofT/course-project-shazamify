0db87f625af5a51df7455d061f9fcfdd820092cf
Star <43018428+Starliyiyu@users.noreply.github.com>
2022-11-07 22:42:54 -0500

    half finished basic user interaction

--- src/main/java/UserInteractABR/FriendManagerABR/FriendManagerResponseModel.java
+++ src/main/java/UserInteractABR/FriendManagerABR/FriendManagerResponseModel.java
@@ -1,4 +1,11 @@
 package UserInteractABR.FriendManagerABR;
 
+import java.util.HashMap;
+
 public class FriendManagerResponseModel {
+    private final String userID;
+
+    public FriendManagerResponseModel(String userID) {
+        this.userID = userID;
+    }
 }
