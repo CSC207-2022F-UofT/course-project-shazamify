6b8dee9d032f4fe1f933ada5f1e9218aa81c8a49
WilsoZhang16 <wilso.zhang@mail.utoronto.ca>
2022-11-08 11:10:09 -0500

    Javadoc

--- src/main/java/queue_entities/HistoryQueueFactory.java
+++ src/main/java/queue_entities/HistoryQueueFactory.java
@@ -5,7 +5,11 @@ import entities.Song;
 import java.util.*;
 
 public class HistoryQueueFactory {
-    //TODO: Add javadocs
+    /***
+     * Produces a HistoryQueue object with an initial song hashmap.
+     * @param songHashMap - A hashmap of songs that are recently played and the percentage of the song played.
+     * @return - A new HistoryQueue object with the song hashmap.
+     */
     public HistoryQueue create(HashMap<Song, Float> songHashMap) {
         return new HistoryQueue(songHashMap);
     }
