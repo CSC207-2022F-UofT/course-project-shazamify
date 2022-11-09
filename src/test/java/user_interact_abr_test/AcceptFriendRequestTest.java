a7dae2fae8db99794621aded55f68f860627eb0f
Star <43018428+Starliyiyu@users.noreply.github.com>
2022-11-08 20:03:00 -0500

    half finished basic user interaction

---
+++ src/test/java/user_interact_abr_test/AcceptFriendRequestTest.java
@@ -0,0 +1,13 @@
+package user_interact_abr_test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AcceptFriendRequestTest {
+
+    @Test
+    void AcceptRequest() {
+
+    }
+}
