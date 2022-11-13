94f3088e140177e2f0d904a6a1889074fca02ea3
wilso <113144236+wilso16@users.noreply.github.com>
2022-11-13 16:26:41 -0500

    Fixed some indentation

--- src/main/java/user/database/UserFileWriter.java
+++ src/main/java/user/database/UserFileWriter.java
@@ -8,12 +8,12 @@ import java.util.Map;
 public class UserFileWriter {
     static public void writeUserMap(Map<String, User> UserMap, String filename){
         try {
-        OutputStream outputStream = new FileOutputStream(filename);
-        OutputStream buffer = new BufferedOutputStream(outputStream);
-        ObjectOutput output = new ObjectOutputStream(buffer);
+            OutputStream outputStream = new FileOutputStream(filename);
+            OutputStream buffer = new BufferedOutputStream(outputStream);
+            ObjectOutput output = new ObjectOutputStream(buffer);
 
-        output.writeObject(UserMap);
-        output.close();
+            output.writeObject(UserMap);
+            output.close();
         } catch(IOException e){
             throw new RuntimeException("Filename of writing file is not correct");
         }
