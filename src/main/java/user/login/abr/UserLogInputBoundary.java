50974c923cd01a1b098a3da822c61d70688a723e
Star <43018428+Starliyiyu@users.noreply.github.com>
2022-11-17 08:32:45 -0500

    Revert "9 feature 7 user interaction system"

--- src/main/java/user/login/abr/UserLogInputBoundary.java
+++
@@ -1,8 +0,0 @@
-package user.login.abr;
-
-import user.login.screen.UserLogViewModel;
-
-public interface UserLogInputBoundary {
-    UserLogViewModel loginUser(UserLogRequestModel requestModel);
-
-}
--- src/main/java/user_interaction/user_interact_abr/manage_friend_request_abr/FriendManagerOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package user_interaction.user_interact_abr.manage_friend_request_abr;
-
-public interface FriendManagerOutputBoundary {
-    FriendManagerResponseModel showMsgAndUpdatedFriendList(FriendManagerResponseModel responseModel);
-}
