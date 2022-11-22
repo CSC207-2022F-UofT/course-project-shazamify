b9da9c6e46bb1e4f4470120430e4a23735c9e842
WilsoZhang16 <wilso.zhang@mail.utoronto.ca>
2022-11-21 21:44:51 -0500

    Added test + javadocs

--- src/main/java/interface_adaptors/queue_ia/QueuePresenter.java
+++ src/main/java/interface_adaptors/queue_ia/QueuePresenter.java
@@ -3,7 +3,9 @@ package interface_adaptors.queue_ia;
 import abr.queue_abr.queue.QueueOutputBoundary;
 import abr.queue_abr.queue.QueueResponseModel;
 
-// Add javadocs
+/***
+ * The queue presenter is responsible for taking the response model list of songs and packiging it for the view model.
+ */
 public class QueuePresenter implements QueueOutputBoundary {
     @Override
     public QueueViewModel present(QueueResponseModel responseModel) {
---
+++ src/test/java/queue_abr_test/UpdateQueueTest.java
@@ -0,0 +1,62 @@
+package queue_abr_test;
+
+import abr.queue_abr.queue.*;
+import entities.queue_entities.*;
+import interface_adaptors.queue_ia.*;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class UpdateQueueTest {
+    @Test
+    public void reorderQueue() {
+        // Initialize the song queue object and create the input data
+        SongQueue songQueue = SongQueue.getInstance();
+        List<String> ids = Arrays.asList("1", "2", "3", "4", "5");
+        songQueue.setQueue(ids);
+
+
+        QueueOutputBoundary queuePresenter = new QueuePresenter();
+        QueueInputBoundary newQueue = new QueueUseCase(queuePresenter);
+        QueueRequestModel newSongList = new QueueRequestModel(Arrays.asList("5", "4", "3", "2", "1"));
+
+        // Runns the use case
+        newQueue.update(newSongList);
+
+        assertEquals(songQueue.getQueue(), Arrays.asList("5", "4", "3", "2", "1"));
+    }
+
+    @Test
+    public void addQueue() {
+        SongQueue songQueue = SongQueue.getInstance();
+        List<String> ids = Arrays.asList("1", "2", "3", "4");
+
+        songQueue.setQueue(ids);
+
+        QueueOutputBoundary queuePresenter = new QueuePresenter();
+        QueueInputBoundary newQueue = new QueueUseCase(queuePresenter);
+        QueueRequestModel newSongList = new QueueRequestModel(Arrays.asList("1", "2", "3", "4", "5"));
+
+        newQueue.update(newSongList);
+
+        assertEquals(songQueue.getQueue(), Arrays.asList("1", "2", "3", "4", "5"));
+    }
+
+    @Test
+    public void removeQueue() {
+        SongQueue songQueue = SongQueue.getInstance();
+        List<String> ids = Arrays.asList("1", "2", "3", "4", "5");
+
+        songQueue.setQueue(ids);
+
+        QueueOutputBoundary queuePresenter = new QueuePresenter();
+        QueueInputBoundary newQueue = new QueueUseCase(queuePresenter);
+        QueueRequestModel newSongList = new QueueRequestModel(Arrays.asList("1"));
+
+        newQueue.update(newSongList);
+
+        assertEquals(songQueue.getQueue(), Arrays.asList("1"));
+    }
+}
