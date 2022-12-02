45040c99da4e31c02d0e321905f2c27758af3a2d
wilson <113144236+wilso16@users.noreply.github.com>
2022-12-02 15:54:59 -0500

    Update SearchEngineDatabaseGateway.java

--- src/main/java/abr/search_engine_abr/SearchEngineDatabaseGateway.java
+++ src/main/java/abr/search_engine_abr/SearchEngineDatabaseGateway.java
@@ -1,6 +1,6 @@
 package abr.search_engine_abr;
 
-import import entities.user_entities.User;;
+import entities.user_entities.User;;
 
 public interface SearchEngineDatabaseGateway {
     User[] getUserArray();
