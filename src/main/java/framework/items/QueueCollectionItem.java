36e2c9438f16c943a78c49230a08be34385eb1a9
Alexander Shchokin <alex.shchokin@hotmail.com>
2022-12-03 15:44:47 -0500

    added stylization and DTO placeholder for QueueCollectionItem (ill clean this up more in the next PR)

--- src/main/java/framework/items/QueueCollectionItem.java
+++ src/main/java/framework/items/QueueCollectionItem.java
@@ -4,6 +4,7 @@ package framework.items;
 import entities.playlist_entities.Playlist;
 import interface_adaptors.MediaPlaylistController;
 import interface_adaptors.SearchResultsViewModel;
+import interface_adaptors.playlist_ia.RecordViewModel;
 import interface_adaptors.queue_ia.QueueViewModel;
 
 import javax.imageio.ImageIO;
@@ -11,6 +12,7 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Queue;
@@ -35,13 +37,18 @@ public class QueueCollectionItem extends JPanel{
         PanelListener listener = new PanelListener();
         this.addMouseListener(listener);
 
+        //TODO: songDTO = ... responseBlahBlahBlah
         try {
-            Image cover = ImageIO.read(playlist.getCover()).getScaledInstance(50,50,Image.SCALE_DEFAULT);
+            Image cover = ImageIO.read(songDTO.getCover()).getScaledInstance(50,50,Image.SCALE_DEFAULT);
             this.add(renderImage(new ImageIcon(cover)));
         }catch(java.io.IOException e){}
 
 
-        this.add(renderLabel(playlist.getName()));
+        this.add(renderLabel(songDTO.getAlbum()));
+        this.add(renderLabel(songDTO.getArtist()));
+        this.add(renderLabel(songDTO.getTitle()));
+        this.add(renderLabel(songDTO.getYear()));
+
 
 
         Border blackline = createMatteBorder(0, 0, 1, 0, new Color(36,36,36));
@@ -70,6 +77,150 @@ public class QueueCollectionItem extends JPanel{
         return coverlabel;
     }
 
+
+    private JPanel renderInputs(){
+        JPanel buttonPanel = new JPanel(new GridLayout());
+        buttonPanel.setOpaque(false);
+        buttonPanel.add(renderPlayButton());
+        //buttonPanel.add(renderMenu());
+        //buttonPanel.add(renderAddButton());
+        buttonPanel.add(renderMenuBar());
+
+        // Add option to remove song from playlist
+        buttonPanel.add(renderDeleteButton());
+
+        // Otherwise add placeholder for xtra space
+        // JLabel placeholder = new JLabel(" ");
+        //placeholder.setOpaque(false);
+        //buttonPanel.add(placeholder);
+        return buttonPanel;
+    }
+    /*
+    private JButton renderAddButton(){
+        JButton button = new JButton();
+        try {
+            button.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/plusiconwhite.png")).getScaledInstance(25, 25, Image.SCALE_DEFAULT)));
+            //button.setBackground(Color.WHITE);
+            button.setOpaque(false);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    handleAddButtonAction(button, e);
+                }
+            });
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        button.setBorderPainted(false);
+        button.setBorder(null);
+        button.setMargin(new Insets(0, 0, 0, 0));
+        button.setContentAreaFilled(false);
+
+        return button;
+    }
+    */
+
+    private JButton renderPlayButton() {
+        final JButton button = new JButton();
+        try {
+            button.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/playiconwhite.png")).getScaledInstance(25, 25, Image.SCALE_DEFAULT)));
+            //button.setBackground(Color.WHITE);
+            button.setOpaque(false);
+            /*button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    handlePlayButtonAction(button, e);
+                }
+            });*/
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        button.setBorderPainted(false);
+        button.setBorder(null);
+        button.setMargin(new Insets(0, 0, 0, 0));
+        button.setContentAreaFilled(false);
+
+
+        return button;
+    }
+
+    public JButton renderDeleteButton(){
+        final JButton button = new JButton();
+        try {
+            button.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/trashiconwhite.png")).getScaledInstance(18, 25, Image.SCALE_DEFAULT)));
+            //button.setBackground(Color.WHITE);
+            button.setOpaque(false);
+            button.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+
+                }
+                @Override
+                public void mouseEntered(MouseEvent e){
+                    try {
+                        button.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/trashiconred.png")).getScaledInstance(18, 25, Image.SCALE_DEFAULT)));
+                    } catch (Exception ex){}
+                }
+                @Override
+                public void mouseExited(MouseEvent e){
+                    try {
+                        button.setIcon(new ImageIcon(ImageIO.read(getClass().getResource("/trashiconwhite.png")).getScaledInstance(18, 25, Image.SCALE_DEFAULT)));
+                    } catch (Exception ex){}
+                }
+            });
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        button.setBorderPainted(false);
+        button.setBorder(null);
+        button.setMargin(new Insets(0, 0, 0, 0));
+        button.setContentAreaFilled(false);
+
+
+        return button;
+    }
+    public JMenuBar renderMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(renderMenu());
+        menuBar.setMinimumSize(new Dimension(50,50));
+        menuBar.setBackground(Color.DARK_GRAY);
+        menuBar.setOpaque(false);
+        return menuBar;
+    }
+
+    public JMenu renderMenu() {
+        JMenu menu = new JMenu();
+        try {
+            ImageIcon plusicon = new ImageIcon(ImageIO.read(getClass().getResource("/plusiconwhite.png")).getScaledInstance(25, 25, Image.SCALE_DEFAULT));
+            menu.setIcon(plusicon);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        menu.setBackground(Color.DARK_GRAY);
+        menu.setOpaque(false);
+        JMenuItem addToQueueMenuItem = new JMenuItem("Add to Queue");
+        JMenuItem addToPlaylistMenuItem = new JMenuItem("Add to Playlist");
+        menu.add(addToQueueMenuItem);
+        menu.add(addToPlaylistMenuItem);
+        return menu;
+    }
+
     private class PanelListener implements MouseListener {
 
         @Override
@@ -80,14 +231,6 @@ public class QueueCollectionItem extends JPanel{
              * then will the object be parsed to JPanel
              * since we need the setBackground() method
              */
-            Object source = event.getSource();
-            if(source instanceof JPanel){
-                JPanel panelPressed = (JPanel) source;
-                panelPressed.setBackground(Color.blue);
-
-                QueueViewModel.getInstance().getView().setVisible(true);
-                SearchResultsViewModel.getInstance().getView().setVisible(false);
-            }
         }
 
         @Override
@@ -116,5 +259,4 @@ public class QueueCollectionItem extends JPanel{
         public void mouseReleased(MouseEvent arg0) {}
 
     }
-
 }
