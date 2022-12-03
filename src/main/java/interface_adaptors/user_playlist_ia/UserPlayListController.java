0608f114b8a987035d2b412b2980d8ae32b45efb
2368883029 <95886018+2368883029@users.noreply.github.com>
2022-12-03 15:51:07 -0500

    Complete UserPlayListController

--- src/main/java/interface_adaptors/user_playlist_ia/UserPlayListController.java
+++ src/main/java/interface_adaptors/user_playlist_ia/UserPlayListController.java
@@ -1,8 +1,28 @@
 package interface_adaptors.user_playlist_ia;
 
+import abr.user_playlist_abr.UserPlaylistInputBoundary;
+import abr.user_playlist_abr.UserPlaylistRequestModel;
+import entities.user_entities.User;
+
 /**
  * @author David Li
  */
 public class UserPlayListController {
+    UserPlaylistInputBoundary inputBoundary;
+    UserPlaylistRequestModel requestModel;
+    public UserPlayListController(UserPlaylistInputBoundary inputBoundary){
+        this.inputBoundary = inputBoundary;
+    }
 
+    public void addPlayListInUser(String userName, String PlayListID){
+        requestModel.setPlayListID(PlayListID);
+        requestModel.setUserName(userName);
+        inputBoundary.addToUserPlaylist(requestModel);
+    }
+
+    public void deletePlaylistInUser(String userName, String PlayListID){
+        requestModel.setPlayListID(PlayListID);
+        requestModel.setUserName(userName);
+        inputBoundary.deleteInUserPlaylist(requestModel);
+    }
 }
