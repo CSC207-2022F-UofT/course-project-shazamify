b4a9aa2f0e9adfd585cfbea6a3dce213be456e16
varygx <allenuy0211@gmail.com>
2022-12-05 14:13:14 -0500

    Added documentation and reformatted code.

--- src/main/java/entities/playlist_entities/Playlist.java
+++ src/main/java/entities/playlist_entities/Playlist.java
@@ -1,20 +1,16 @@
 package entities.playlist_entities;
 
 import entities.Record;
-import entities.Song;
 
 import java.io.File;
 import java.util.ArrayList;
 
-;
-
 public class Playlist implements Record {
+    private final String owner;
+    private final String id;
     private String name;
-
-    private String id;
     private int duration;
     private ArrayList<String> contents;
-    private final String owner;
     private String description;
     private Privacy privacy;
 
@@ -33,6 +29,7 @@ public class Playlist implements Record {
         this.privacy = Privacy.PRIVATE;
 
     }
+
     //TODO: initialize with default names
     @Override
     public String getName() {
@@ -53,62 +50,75 @@ public class Playlist implements Record {
     public int getDuration() {
         return this.duration;
     }
+
     public String setDuration(int duration) {
         this.duration = duration;
         return this.getId() + " duration: " + this.getDuration();
     }
 
-    public String getArtist(){return this.owner;}
-    public String getDescription(){return this.description;}
-    public String setDescription(String description){
+    public String getArtist() {
+        return this.owner;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String setDescription(String description) {
         this.description = description;
         return this.getId() + " description: " + this.getDescription();
     }
-    public Privacy getPrivacy(){
+
+    public Privacy getPrivacy() {
         return this.privacy;
     }
-    public String setPrivacy(Privacy privacy){
+
+    public String setPrivacy(Privacy privacy) {
         this.privacy = privacy;
         return this.getId() + " privacy: " + this.getPrivacy();
     }
-    public File getCover() {return null;}
-    public ArrayList<String> getSongs() {return this.contents;}
-    public Object setSongs(ArrayList<String> contents){this.contents = contents;
+
+    public File getCover() {
         return null;
     }
-    public boolean reOrderSongs(String songID, int ind){
-        if(this.contents.contains(songID) && ind < this.contents.size()){
+
+    public ArrayList<String> getSongs() {
+        return this.contents;
+    }
+
+    public Object setSongs(ArrayList<String> contents) {
+        this.contents = contents;
+        return null;
+    }
+
+    public boolean reOrderSongs(String songID, int ind) {
+        if (this.contents.contains(songID) && ind < this.contents.size()) {
             this.deleteSong(songID);
             this.addSong(songID, ind);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public void deleteSong(String songID){
-        if(this.contents.contains(songID)){
-            this.contents.remove(songID);
-        }
+    public void deleteSong(String songID) {
+        this.contents.remove(songID);
     }
 
-    public boolean addSong(String songID){
-        if (this.contents.contains(songID)){
+    public boolean addSong(String songID) {
+        if (this.contents.contains(songID)) {
             return false;
-        }
-        else{
+        } else {
             this.contents.add(songID);
             return true;
         }
     }
 
-    public boolean addSong(String songID, int ind){
-        if(!(this.contents.contains(songID)) && ind < this.contents.size()){
+    public boolean addSong(String songID, int ind) {
+        if (!(this.contents.contains(songID)) && ind < this.contents.size()) {
             this.contents.add(ind, songID);
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
