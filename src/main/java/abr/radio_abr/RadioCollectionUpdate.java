0d69a8f4abdae644ff31c92c7004c618ca54202a
Cynthia Sa <cynthia.sa.2108@gmail.com>
2022-12-07 15:11:21 -0500

    Let's try this again

--- src/main/java/abr/radio_abr/RadioCollectionUpdate.java
+++ src/main/java/abr/radio_abr/RadioCollectionUpdate.java
@@ -2,21 +2,31 @@ package abr.radio_abr;
 
 
 import entities.radio_entities.FavouriteRadioCollection;
+import entities.radio_entities.RadioStation;
 
 /***
  * @author cynth
+ * @since 2022-12-01
  */
 public class RadioCollectionUpdate {
     // The purpose of this class is to update the collection of Radio Stations.
 
-    // Creating a method to add a new Radio Station to the HashMap.
-    public static void addStation(String name, String id){
-        // Will need to take the ID from the API as well as Name.
-        FavouriteRadioCollection.favouriteRadioStations.put(id, name);
+    /***
+     *  Adds a RadioStation object to the Favourite Radio Station collection hashmap. The key is set as the station's name.
+     *  @param station RadioStation
+     */
+    public static void addStation(RadioStation station){
+        String name = station.getName();
+        FavouriteRadioCollection.favouriteRadioStations.put(name, station);
     }
 
+    /***
+     *  Removes a RadioStation object to the Favourite Radio Station collection hashmap. Uses the key - the station's name - to locate
+     *  and remove the object.
+     *  @param name String
+     */
     // Creating a method to remove a Radio Station from the HashMap.
-    public static void removeStation(String id){
-        FavouriteRadioCollection.favouriteRadioStations.remove(id);
+    public static void removeStation(String name){
+        FavouriteRadioCollection.favouriteRadioStations.remove(name);
     }
 }
