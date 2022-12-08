8111a125de5593980d3a1f2c107c5797c0137658
dat <tiendat691@gmail.com>
2022-12-08 02:12:46 -0500

    update playlistCreate to take names

---
+++ src/test/java/Playlist/CreatePlaylistTest.java
@@ -0,0 +1,17 @@
+package Playlist;
+
+import abr.playlist_abr.PlaylistCreateInputBoundary;
+import entities.playlist_entities.Playlist;
+import org.junit.Test;
+
+public class CreatePlaylistTest {
+
+    public CreatePlaylistTest(){
+
+    }
+    @Test
+    public void createPlaylist(){
+        PlaylistCreateInputBo
+    }
+
+}
