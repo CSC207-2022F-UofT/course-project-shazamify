d456b2727d961838f990e424fe618e2b391b3cbd
Alexander Shchokin <alex.shchokin@hotmail.com>
2022-12-08 01:14:23 -0500

    minor fixes

---
+++ src/main/java/framework/items/AddToPlaylistMenuItem.java
@@ -0,0 +1,28 @@
+package framework.items;
+
+import interface_adaptors.playlist_ia.PlaylistModifyControl;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class AddToPlaylistMenuItem extends JMenuItem {
+    public AddToPlaylistMenuItem(String name, String playlistId, String songId){
+        this.setText(name);
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                PlaylistModifyControl playlistModifier = new PlaylistModifyControl();
+
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+    }
+}
